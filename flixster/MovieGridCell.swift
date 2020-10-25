5912959d3fd5eb8d959eb39b98925167ed9b3779
Matin Ghaffari <matinghaffari@yahoo.com>
2020-10-25 13:09:22 -0700

    Flix Pt2 complete
    Completed Part 2 of Flix app.

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Matin Ghaffari on 10/24/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
